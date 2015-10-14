57defb7da96e7650b4a763b6fe6886a84c073019
Edouard DUPIN <yui.heero@gmail.com>
2015-10-14 21:21:03 +0200

    [DEV] update new lutin 0.8.0

--- lutin_enet-test.py
+++ lutin_enet-test.py
@@ -2,28 +2,36 @@
 import lutin.module as module
 import lutin.tools as tools
 
+
+def get_type():
+	return "BINARY"
+
+def get_sub_type():
+	return "TEST"
+
 def get_desc():
 	return "e-net TEST test software for enet"
 
 def get_licence():
-	return {
-		"assimilate":"APACHE2",
-		"type":"APACHE-2.0"
-		}
+	return "APACHE-2"
 
-def create(target):
-	my_module = module.Module(__file__, 'enet-test', 'BINARY')
-	
-	my_module.add_module_depend(['enet', 'gtest'])
-	
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
+	my_module.add_module_depend(['enet', 'gtest', 'test-debug'])
 	my_module.add_src_file([
 		'test/debug.cpp',
 		'test/main.cpp'
 		])
-	
 	my_module.add_export_path(tools.get_current_path(__file__))
-	
-	# add the currrent module at the 
 	return my_module
 
 
--- lutin_enet.py
+++ lutin_enet.py
@@ -2,20 +2,31 @@
 import lutin.module as module
 import lutin.tools as tools
 
+
+def get_type():
+	return "LIBRARY"
+
 def get_desc():
-	return "e-net TCP/UDP/HTTP/FTP interface"
+	return "TCP/UDP/HTTP/FTP interface"
 
 def get_licence():
-	return {
-		"assimilate":"APACHE2",
-		"type":"APACHE-2.0"
-		}
+	return "APACHE-2"
 
-def create(target):
-	my_module = module.Module(__file__, 'enet', 'LIBRARY')
-	
+def get_compagny_type():
+	return "com"
+
+def get_compagny_name():
+	return "atria-soft"
+
+def get_maintainer():
+	return ["Mr DUPIN Edouard <yui.heero@gmail.com>"]
+
+def get_version():
+	return [0,0,0]
+
+def create(target, module_name):
+	my_module = module.Module(__file__, module_name, get_type())
 	my_module.add_module_depend(['etk'])
-	
 	my_module.add_src_file([
 		'enet/debug.cpp',
 		'enet/Udp.cpp',
@@ -23,10 +34,14 @@ def create(target):
 		'enet/Http.cpp',
 		'enet/Ftp.cpp',
 		])
-	
-	my_module.add_export_path(tools.get_current_path(__file__))
-	
-	# add the currrent module at the 
+	my_module.add_header_file([
+		'enet/debug.h',
+		'enet/Udp.h',
+		'enet/Tcp.h',
+		'enet/Http.h',
+		'enet/Ftp.h',
+		])
+	my_module.add_path(tools.get_current_path(__file__))
 	return my_module
 
 
